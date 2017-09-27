578ae059488e4bd746b18716d301500262fa632d
VincentHwuang <Vincent_Hwuang@hotmail.com>
2017-09-27 08:59:58 +0800

    committed at 08:59 09-27

--- Matrix/SparseMat/sparsemat.h
+++ Matrix/SparseMat/sparsemat.h
@@ -25,10 +25,41 @@ class CSparseMat
 			int CF;	//Column Flag
 			T	Payload;
 		};
-		NonZeroElemT *m_pElems;
-		int m_RNum;
-		int m_CNum;
-		int m_NonZeroElemNum;
+		NonZeroElemT *m_pElems{};
+		int m_RNum{};
+		int m_CNum{};
+		int m_NonZeroElemNum{};
+#ifdef ROW_LOGICAL_LINKED
+		int *m_pRpos{};	//Used for row-logical-linked representation while implementing multiply operation.
+						//Records indices of the first non-zero element of each row in 'm_pElems'.
+		//Method to calculate values for 'm_pRpos'
+		void CalcuRpos(void)
+		{
+			if(m_pRpos != NULL)
+			{
+				delete m_pRpos;
+			}
+			try
+			{
+				m_pRpos=new int[m_RNum+1]{};
+			}
+			catch(std::bad_alloc& e)
+			{
+				std::cout<<"Failed to allocate memory for 'm_pRpos'!"<<std::endl;
+				exit(-1);
+			}
+			int Num[m_RNum+1]{};
+			for(int i{1};i <= m_NonZeroElemNum;i++)
+			{
+				++Num[m_pElems[i].RF];				
+			}
+			m_pRpos[1]=1;
+			for(int j{2};j <= m_RNum;j++)
+			{
+				m_pRpos[j]=m_pRpos[j-1]+Num[j-1];
+			}
+		}
+#endif
 	public:
 		//Constructor function
 		CSparseMat(int RNum,int CNum,int NonZeroElemNum) :
@@ -36,11 +67,11 @@ class CSparseMat
 		{
 			try
 			{
-				m_pElems=new NonZeroElemT[NonZeroElemNum+1];
+				m_pElems=new NonZeroElemT[NonZeroElemNum+1]{};
 			}
 			catch (std::bad_alloc& e)
 			{
-				std::cout<<"Failed to allocate memory!"<<std::endl;
+				std::cout<<"Failed to allocate memory for 'm_pElems'!"<<std::endl;
 				exit(-1);
 			}
 		}
@@ -59,7 +90,22 @@ class CSparseMat
 					}
 				}
 			}
-			CSparseMat<T> Temp{RNum,CNum,NonZeroElemNum};
+			if(this->m_pElems != NULL)
+			{
+				delete this->m_pElems;
+			}
+			try
+			{
+				this->m_pElems=new NonZeroElemT[NonZeroElemNum+1];
+			}
+			catch(std::bad_alloc& e)
+			{
+				std::cout<<"Failed to allocate memory!"<<std::endl;
+				exit(-1);
+			}
+			this->m_RNum=RNum;
+			this->m_CNum=CNum;
+			this->m_NonZeroElemNum=NonZeroElemNum;
 			int k{1};
 			for(int i{};i < RNum ;i++)
 			{
@@ -67,28 +113,23 @@ class CSparseMat
 				{
 					if(pElems[i*CNum+j] != 0)
 					{	
-						Temp.m_pElems[k].Payload=pElems[i*CNum+j];
-						Temp.m_pElems[k].RF=i+1;
-						Temp.m_pElems[k].CF=j+1;
+						this->m_pElems[k].Payload=pElems[i*CNum+j];
+						this->m_pElems[k].RF=i+1;
+						this->m_pElems[k].CF=j+1;
 						k++;
 					}
 				}
 			}
-			*this=Temp;			
+#ifdef ROW_LOGICAL_LINKED
+			CalcuRpos();
+#endif			
 		}
 		CSparseMat(const CSparseMat& SparseMat)
 		{
-			*this=SparseMat;
-		}
-		//Destructor function
-		~CSparseMat()
-		{
-			if(m_pElems != NULL)
-				delete m_pElems;
-		}
-		//Implementation of assignment overriding operation
-		void operator=(const CSparseMat &SparseMat)
-		{
+			if(this->m_pElems != NULL)
+			{
+				delete this->m_pElems;
+			}
 			try
 			{
 				this->m_pElems=new NonZeroElemT[SparseMat.m_NonZeroElemNum+1];
@@ -107,32 +148,74 @@ class CSparseMat
 			this->m_RNum=SparseMat.m_RNum;
 			this->m_CNum=SparseMat.m_CNum;
 			this->m_NonZeroElemNum=SparseMat.m_NonZeroElemNum;
+#ifdef ROW_LOGICAL_LINKED
+			CalcuRpos();
+#endif
+		}
+		//Destructor function
+		~CSparseMat()
+		{
+			if(m_pElems != NULL)
+				delete m_pElems;
+#ifdef ROW_LOGICAL_LINKED
+			if(m_pRpos != NULL)
+				delete m_pRpos;
+#endif
+		}
+		//Implementation of assignment overriding operation
+		void operator=(const CSparseMat &SparseMat)
+		{
+			if(this->m_pElems != NULL)
+			{
+				delete this->m_pElems;
+			}
+			try
+			{
+				this->m_pElems=new NonZeroElemT[SparseMat.m_NonZeroElemNum+1];
+			}
+			catch (std::bad_alloc& e)
+			{
+				std::cout<<"Failed to allocate memory!"<<std::endl;
+				exit(-1);
+			}
+			for(int i{1};i <= SparseMat.m_NonZeroElemNum;i++)
+			{
+				this->m_pElems[i].Payload=SparseMat.m_pElems[i].Payload;
+				this->m_pElems[i].RF=SparseMat.m_pElems[i].RF;
+				this->m_pElems[i].CF=SparseMat.m_pElems[i].CF;
+			}
+			this->m_RNum=SparseMat.m_RNum;
+			this->m_CNum=SparseMat.m_CNum;
+			this->m_NonZeroElemNum=SparseMat.m_NonZeroElemNum;
+#ifdef ROW_LOGICAL_LINKED
+			CalcuRpos();
+#endif
 		}
 		//Implementation of transposing the matrix,and return its result
 		CSparseMat& Transpose()
 		{
 			/* Using the most normal method to obtain the transpose matrix with triple representation storing,time consuming of this algorithm is O(m_CNum*m_NonZeroElemNum),so when the number of non-zero elements is at the same quantity level with 'm_RNum*m_CNum',the time consuming is O(m_RNum*(m_CNum^2)),so this algorithm apply to such condition:m_NonZeroElemNum << m_RNum * m_CNum */
-//			CSparseMat Temp{*this};
-//			std::swap(m_RNum,m_CNum);
-//			if(m_NonZeroElemNum != 0)
-//			{
-//				int Index{1};
-//				for(int Col{1}; Col <= Temp.m_CNum; Col++)
-//				{
-//					for(int Index1{1}; Index1 <= Temp.m_NonZeroElemNum; Index1++)
-//					{
-//						if(Temp.m_pElems[Index1].CF == Col)
-//						{
-//							m_pElems[Index].RF=Temp.m_pElems[Index1].CF;
-//							m_pElems[Index].CF=Temp.m_pElems[Index1].RF;
-//							m_pElems[Index].Payload=Temp.m_pElems[Index1].Payload;
-//							++Index;
-//						}
-//					}
-//				}
-//			}
-
+			CSparseMat Temp{*this};
+			std::swap(m_RNum,m_CNum);
+			if(m_NonZeroElemNum != 0)
+			{
+				int Index{1};
+				for(int Col{1}; Col <= Temp.m_CNum; Col++)
+				{
+					for(int Index1{1}; Index1 <= Temp.m_NonZeroElemNum; Index1++)
+					{
+						if(Temp.m_pElems[Index1].CF == Col)
+						{
+							m_pElems[Index].RF=Temp.m_pElems[Index1].CF;
+							m_pElems[Index].CF=Temp.m_pElems[Index1].RF;
+							m_pElems[Index].Payload=Temp.m_pElems[Index1].Payload;
+							++Index;
+						}
+					}
+				}
+			}
 			/* Another more efficient algorithm named quick transpose, add two vector:Num[] and Cpot[],Num[] stores the number of non-zero elements of each column,and the vector Cpot[] store the index of the first non-zero element of each column in m_pElems,because i don't use the first location in m_pElems,so Cpot[1]=1.Time consuming of this algorithm is O(m_CNum+m_NonZeroElemNum),when the number of non-zero elements is at the same quantity level with 'm_RNum * m_CNum',the time consuming is O(m_RNum*m_CNum) */
+#ifdef QUICK_TRANSPOSE
 			CSparseMat Temp{*this};
 			int Num[m_CNum+1];
 			int Cpot[m_CNum+1];
@@ -165,7 +248,76 @@ class CSparseMat
 					++Cpot[Col];
 				}
 			}
+#endif
 			return *this;
+		}
+		//Method to implement multiply operation using row-logical-linked storing representation
+		CSparseMat operator*(const CSparseMat& SparseMat)
+		{
+			CSparseMat Temp{this->m_RNum,SparseMat.m_CNum,this->m_RNum*SparseMat.m_CNum};
+			int RowFlag{1};
+			int ColumnFlag0{1};
+			int ColumnFlag1{1};
+			int CTemp[SparseMat.m_CNum+1]{};
+			int RowBound0{};
+			int RowBound1{};
+			int NonZeroElemNum{1};
+
+			if(this->m_NonZeroElemNum * SparseMat.m_NonZeroElemNum != 0)
+			{
+				//Handle each row in turn
+				for(;RowFlag <= (this->m_RNum);RowFlag++)	
+				{	
+					//Clear the 'CTemp' array
+					for(int i{1};i <= SparseMat.m_CNum;i++)
+					{
+						CTemp[i]=0;
+					}
+					//Temp.m_pRpos[RowFlag]=NonZeroElemNum+1;
+					//Calculate the bound of current row
+					if(RowFlag < (this->m_RNum))	//It is not the last row
+					{
+						RowBound0=this->m_pRpos[RowFlag+1];
+					}
+					else							//It is the last row
+					{
+						RowBound0=this->m_NonZeroElemNum+1;
+					}
+					//Handle each element in current row
+					for(int j{this->m_pRpos[RowFlag]};j < RowBound0;j++)
+					{
+						//Obtain the column number of current element
+						ColumnFlag0=this->m_pElems[j].CF;
+						//Calculate the bound of corresponding row in the second matrix
+						if(ColumnFlag0 < (SparseMat.m_RNum))
+						{
+							RowBound1=SparseMat.m_pRpos[ColumnFlag0+1];
+						}
+						else
+						{
+							RowBound1=SparseMat.m_NonZeroElemNum+1;
+						}
+						for(int k{SparseMat.m_pRpos[ColumnFlag0]};k < RowBound1;k++)
+						{
+							ColumnFlag1=SparseMat.m_pElems[k].CF;
+							CTemp[ColumnFlag1] += this->m_pElems[j].Payload*SparseMat.m_pElems[k].Payload;
+						}
+					}
+					for(ColumnFlag1=1;ColumnFlag1 <= Temp.m_CNum;ColumnFlag1++)
+					{
+						if(CTemp[ColumnFlag1] != 0)
+						{
+							Temp.m_pElems[NonZeroElemNum].RF=RowFlag;
+							Temp.m_pElems[NonZeroElemNum].CF=ColumnFlag1;
+							Temp.m_pElems[NonZeroElemNum].Payload=CTemp[ColumnFlag1];
+							NonZeroElemNum++;
+						}
+					}
+				}
+			}
+			Temp.m_NonZeroElemNum=NonZeroElemNum-1;
+			Temp.CalcuRpos();
+			return Temp;
 		}
 
 		//Debug function
@@ -176,8 +328,15 @@ class CSparseMat
 			{
 				std::cout<<m_pElems[i].RF<<"\t"<<m_pElems[i].CF<<"\t"<<m_pElems[i].Payload<<std::endl;
 			}
+#ifdef ROW_LOGICAL_LINKED
+			std::cout<<"m_pRpos:"<<std::endl;
+			for(int j{1};j <= m_RNum;j++)
+			{
+				std::cout<<m_pRpos[j]<<"\t";
+			}
+			std::cout<<std::endl;
+#endif
 		}
-
 };
 }
 
--- Matrix/SparseMat/test.cpp
+++ Matrix/SparseMat/test.cpp
@@ -4,17 +4,28 @@ using ds::CSparseMat;
 
 int main(void)
 {
-	int Elems[4][3]=
+	int SparseMat0[3][4]=
 	{
-		{1,2,3}, 
-		{0,4,0}, 
-		{5,6,0}, 
-		{0,0,7}
+		{3, 1,0,5 }, 
+		{0,-1,0,0 }, 
+		{2, 0,0,0 }, 
 	};
-	CSparseMat<int> SparseMat{4,3,(int*)Elems};
-//	SparseMat.PrintInfo();
-	CSparseMat<int> SparseMat1{SparseMat.Transpose()};
-	SparseMat1.PrintInfo();
+	int SparseMat1[4][2]=
+	{
+		{ 9,2},
+		{ 1,0},
+		{-2,4},
+		{ 0,0},
+	};
+	CSparseMat<int> SparseMatrix0{3,4,(int*)SparseMat0};
+	CSparseMat<int> SparseMatrix1{4,2,(int*)SparseMat1};
+	SparseMatrix0.PrintInfo();
+	SparseMatrix1.PrintInfo();
+	SparseMatrix0=SparseMatrix0*SparseMatrix1;
+	SparseMatrix0.PrintInfo();
+
+//	CSparseMat<int> SparseMat1{SparseMat.Transpose()};
+//	SparseMat1.PrintInfo();
 //	SparseMat.Transpose();
 //	SparseMat.PrintInfo();
 
